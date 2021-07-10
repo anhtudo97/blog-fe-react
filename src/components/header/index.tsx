33c1ec31628417b0d7a83b81127dd30fb358799c
anhtudo97 <anhtudo97@gmail.com>
2021-07-11 00:52:14 +0700

    feat: header components

---
+++ src/components/header/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  HeaderContainer,
+  HeaderTitle,
+  HeaderTitleSm,
+  HeaderTitleLg,
+  HeaderImg,
+} from './styled';
+
+export default function Header() {
+  return (
+    <HeaderContainer>
+      <HeaderTitle>
+        <HeaderTitleSm>React & Node</HeaderTitleSm>
+        <HeaderTitleLg>Blog</HeaderTitleLg>
+      </HeaderTitle>
+      <HeaderImg
+        src="https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        alt="bg"
+      />
+    </HeaderContainer>
+  );
+}
